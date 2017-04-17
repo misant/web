3ea92590e2e0d5d9592d95f3b863cef609320a94
root <root@localhost.localdomain>
2017-04-18 00:29:24 +0300

    RU lang adding stated

---
+++ app/lang/ru/dashboard.php
@@ -0,0 +1,30 @@
+<?php
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Dashboard Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used on the dashboard page.
+	|
+	*/
+
+	'activity' => 'Активность',
+		
+	'selectView' => 'Выбери вид',
+		
+	'liveView' => 'Прямая трансляция',
+		
+	'lastActivity' => 'Последняя активность',
+		
+	'heatmap' => 'Тепловая карта',
+		
+	'overview' => 'Обзор',
+		
+	'hour' => 'По часам',
+		
+	'average' => 'Среднее',
+		
+	'weekday' => 'Дни недели' 
+);
---
+++ app/lang/ru/general.php
@@ -0,0 +1,58 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| General Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used on the master template.
+	|
+	*/
+
+	'dashboard' => 'Главная',
+
+	'system' => 'Система',
+
+	'settings' => 'Настройки',
+
+	'configuration' => 'Конфигурация',
+
+	'cloud' => 'Облако',
+    
+	'on' => 'Вкл.',
+    
+	'off' => 'Выкл.',
+    
+	'logout' => 'Выйти',
+
+	'update-profile' => 'Обновить профиль',
+    
+    /*
+	|------------------------
+    |  Date-time translations
+	|------------------------
+    */
+
+    'monday' => 'Понедельник',
+
+    'tuesday' => 'Вторник',
+
+    'wednesday' => 'Среда',
+
+    'thursday' => 'Четверг',
+
+    'friday' => 'Пятница',
+
+    'saturday' => 'Суббота',
+
+    'sunday' => 'Воскресенье',
+            
+    'today' => 'Сегодня',
+    
+    'yesterday' => 'Вчера',
+    
+    'dayBeforeYesterday' => 'Позавчера'
+
+);
